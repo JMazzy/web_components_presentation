d2376115e931b09363cde0786a855f7d67a0d388
JMazzy <j.masland@gmail.com>
2016-09-06 15:18:28 -0400

    comments in example files

--- js/custom_element_example.js
+++ js/custom_element_example.js
@@ -6,8 +6,10 @@ APP.CustomElementExample = (function() {
     // declaring an element - MUST HAVE A DASH (-) IN THE NAME!!!
     var ThingyThing = document.registerElement('thingy-thing');
 
+    // get a handle on the element in the main document
     var thingyThing = document.querySelector('thingy-thing');
 
+    // add a click listener
     thingyThing.addEventListener('click', function() {
       alert("You clicked a ThingyThing!!!");
     });
--- js/template_example.js
+++ js/template_example.js
@@ -4,6 +4,7 @@ APP.TemplateExample = (function() {
   var dogs, t, name, breed;
 
   function init() {
+    // sample data for the template
     dogs = [
       { name: "Fluffy", breed: "Doberman" },
       { name: "Fido", breed: "Wolfhound" },
@@ -12,19 +13,27 @@ APP.TemplateExample = (function() {
       { name: "Rex", breed: "Labrador Retriever" }
     ];
 
+    // get a handle on the template
     t = document.querySelector("#dog-table-row");
 
     for ( var i = 0; i < dogs.length; i++ ) {
       var dog = dogs[i];
+
+      // fields where data will go
       nameElement = t.content.querySelector(".dog-name");
       breedElement = t.content.querySelector(".dog-breed");
 
+      // fill in the data
       nameElement.textContent = dog.name;
       breedElement.textContent = dog.breed;
 
+      // get a handle on the table body in the main document
       var tbody = document.querySelector("#dog-table-body");
+
+      // "clone" template and insert content
       var clone = document.importNode(t.content, true);
 
+      // add the cloned row to the table body
       tbody.appendChild(clone);
     }
   }
